7397de34534278042dcd6ac6adf348e90e5da400
tomato <ljc6qx@163.com>
2021-11-04 19:01:49 +0800

    Init code

---
+++ README.md
@@ -0,0 +1,50 @@
+
+# Newrelic Logs
+NewRelic logs is a NodeJS library for send winston logs to newrelic
+
+## Install
+```console
+$ npm i newrelic-logs -S
+```
+or
+```console
+$ yarn add newrelic-logs
+```
+
+## Options
+- **apiKey**: Your newrelic api key *[required]*
+- **appName**: The application name *[required]*
+- **level**: The log level of the application or service
+- **intakeRegion**: The datadog intake to use. set to `eu` to force logs to be sent to the EU specific intake
+
+## Alternatively, you can use environment variable to pass apiKey and appName
+## Add Environment Variables
+
+```node
+process.env.NEW_RELIC_LICENSE_KEY = 'my-key'
+process.env.NEW_RELIC_APP_NAME =  'my-app-name'
+```
+
+## Usage
+```javascript
+const { createLogger } = require('winston')
+const NewrelicTransport = require('newrelic-logs')
+
+// Create a logger and consume an instance of your transport
+const logger = createLogger({
+  // Whatever options you need
+  // Refer https://github.com/winstonjs/winston#creating-your-own-logger
+})
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(
+        new NewrelicTransport({
+        apiKey: 'YOUR_NEW_RELIC_LICENSE_KEY',
+        appName: 'YOUR_APP_NAME',
+        level: 'info',
+    })
+  )
+}
+
+```
+
---
+++ src/index.js
@@ -0,0 +1,81 @@
+const Transport = require('winston-transport')
+const newrelic = require('newrelic')
+const fetch = require('node-fetch')
+
+/**
+ * Class for sending logging information to Newrelic's HTTPS intakes
+ * @extends Transport
+ */
+module.exports = class NewrelicTransport extends Transport {
+  /**
+   * Constructor for the Newrelic Transport responsible for making
+   * HTTP requests whenever log messages are received
+   * @param {!Object} opts Transport options
+   * @param {string} opts.apiKey The Newrelic API key
+   * @param {string} [intakeRegion] The intake region to be used
+   */
+  constructor (opts = {}) {
+    super(opts)
+
+    if (!opts.apiKey) {
+      throw new Error('Missing required option: `apiKey`')
+    }
+    if (opts.intakeRegion === 'eu') {
+      this.api = `https://log-api.eu.newrelic.com/log/v1`
+    } else {
+      this.api = `https://log-api.newrelic.com/log/v1/${opts.apiKey}`
+    }
+    this.apiKey = opts.apiKey || process.env.NEW_RELIC_LICENSE_KEY
+    this.appName = opts.appName || process.env.NEW_RELIC_APP_NAME
+    this.level = opts.level || 'info'
+  }
+
+  /**
+   * Expose the name of the Transport
+   */
+  get name () {
+    return 'Newrelic Logs'
+  }
+
+  /**
+   * Core logging method exposed to Winston
+   * @param {!Object} info Information to be logged
+   * @param {function} callback Continuation to respond when complete
+   */
+  async log (info, callback) {
+    setImmediate(() => {
+      this.emit('logged', info)
+    })
+
+    try {
+
+      let traceData = newrelic.getLinkingMetadata()
+
+      const requestBody = {
+          "message": info,
+          "level": this.level,
+          "timestamp": Date.now(),
+          ...traceData
+      }
+
+      const config = {
+          method: 'POST',
+          headers: {
+              'Content-Type': "application/json",
+              'X-License-Key': this.apiKey
+          },
+          body: JSON.stringify(requestBody)
+      }
+
+      const response = await fetch(this.api, config)
+      if (!response.ok) throw Error(response.message);
+
+      // const data = await response.json();
+      // return data;
+    } catch (err) {
+      console.log(`Error clear log in new-relic ${this.appName}: ${error}`);
+    } finally {
+      callback()
+    }
+  }
+}
